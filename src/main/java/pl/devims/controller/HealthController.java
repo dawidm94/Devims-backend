0ebf4372b0893335fcbe24c217cf328cc366ba43
Dawid Marcinków <dawid.marcinkow@gmail.com>
2022-04-28 11:40:13 +0200

    Rollback

--- src/main/java/pl/devims/controller/HealthController.java
+++ src/main/java/pl/devims/controller/HealthController.java
@@ -1,6 +1,5 @@
 package pl.devims.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,17 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthController {
 
-    @Value("${spring.datasource.url}")
-    String dataSourceUrl;
-
-    @Value("${spring.datasource.username}")
-    String username;
-
-    @Value("${spring.datasource.password}")
-    String pw;
-
     @GetMapping()
     public ResponseEntity<String> getVersion() {
-        return ResponseEntity.ok("I am alive!!, url: [" + dataSourceUrl + "], username: [" + username + "], password: [" + pw + "].");
+        return ResponseEntity.ok("I am alive!!");
     }
 }
