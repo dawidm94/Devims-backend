50cbf4a1a3322f0b9ba9655f0db8f47c16a24a3d
Dawid Marcinków <dawid.marcinkow@gmail.com>
2022-04-28 11:35:28 +0200

    show db credentials

--- src/main/java/pl/devims/controller/HealthController.java
+++ src/main/java/pl/devims/controller/HealthController.java
@@ -1,5 +1,6 @@
 package pl.devims.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthController {
 
+    @Value("spring.datasource.url")
+    String dataSourceUrl;
+
+    @Value("spring.datasource.username")
+    String username;
+
+    @Value("spring.datasource.password")
+    String pw;
+
     @GetMapping()
     public ResponseEntity<String> getVersion() {
-        return ResponseEntity.ok("I am alive!!");
+        return ResponseEntity.ok("I am alive!!, url: [" + dataSourceUrl + "], username: [" + username + "], password: [" + pw + "].");
     }
 }
