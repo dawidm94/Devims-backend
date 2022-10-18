577e24a6dd9890f460b70bfc6bcaf2d0d9d8907e
Dawid Marcinków <dawid.marcinkow@gmail.com>
2022-10-18 08:46:12 +0200

    Season earnings - seperated by team

--- src/main/java/pl/devims/entity/EsorEarnings.java
+++ src/main/java/pl/devims/entity/EsorEarnings.java
@@ -1,21 +1,26 @@
 package pl.devims.entity;
 
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.UpdateTimestamp;
 import pl.devims.dto.DtoEsorNomination;
 import pl.devims.model.ProcessStatus;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 @Table(name = "esor_earnings")
-@NoArgsConstructor
 @Data
+@TypeDef(name = "json", typeClass = JsonStringType.class)
 public class EsorEarnings {
 
     @Id
@@ -24,10 +29,30 @@ public class EsorEarnings {
     private double alreadyEarnedAmount;
     private double futureNominationsAmount;
 
+    @Type( type = "json" )
+    @Column(columnDefinition="TEXT")
+    private Map<String, Double> teamPayments;
+
     @UpdateTimestamp
     private LocalDateTime lastModifiedDateTime;
 
+    public EsorEarnings() {
+        this.teamPayments = new HashMap<>();
+    }
+
     public void addAmount(DtoEsorNomination nomination) {
+        String teamHome = nomination.getTeamHome().trim();
+
+        Double teamHomeSumAmount = this.teamPayments.get(teamHome) != null
+                ? (this.teamPayments.get(teamHome) + nomination.getToPay())
+                : nomination.getToPay();
+
+        this.teamPayments.put(teamHome, teamHomeSumAmount);
+
+        addToSum(nomination);
+    }
+
+    private void addToSum(DtoEsorNomination nomination) {
         if (LocalDate.now().isAfter(nomination.getDate())) {
             this.alreadyEarnedAmount += nomination.getToPay();
 
